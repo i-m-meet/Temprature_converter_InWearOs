ddff86fe5bc76e45a733bcd462ab9147a9d97309
Sumeet Sumeet <Sumeet5990@conestogac.on.ca>
2024-09-28 15:18:28 -0400

    java code completed

--- app/src/main/java/com/example/sumeetassignment1/MainActivity.java
+++ app/src/main/java/com/example/sumeetassignment1/MainActivity.java
@@ -1,24 +1,92 @@
 package com.example.sumeetassignment1;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.sumeetassignment1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private boolean isCelsiusSelected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Initializing View Binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Default Color of buttons is blue
+        defaultButtonColors();
+
+        // Set Click Listeners
+        binding.buttonCelsius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCelsiusSelected = true;
+                // To change color of selected button from blue to dark_blue
+                updateButtonColors();
+                //Conversion function called
+                convertTemprature();
+            }
+        });
+
+        binding.buttonFahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCelsiusSelected = false;
+                updateButtonColors();
+                convertTemprature();
+            }
         });
     }
+
+    //Convert temperature on the basis of selected button
+    private void convertTemprature() {
+        String tempInput = binding.editTextTemperature.getText().toString();
+        if (!tempInput.isEmpty()) {
+            double inputTemp = Double.parseDouble(tempInput);
+            if (isCelsiusSelected) {
+                //Convert Celsius into Fahrenheit
+                double convertedTemp = (inputTemp * 9 / 5) + 32;
+                binding.textViewConvertedTemp.setText(String.format("%.2f ℉", convertedTemp));
+            } else {
+                //Convert Fahrenheit into Celsius
+                double convertedTemp = (inputTemp - 32) * 5 / 9;
+                binding.textViewConvertedTemp.setText(String.format("%.2f ℃", convertedTemp));
+            }
+        } else {
+            // empty output text view if no input is given
+            binding.textViewConvertedTemp.setText("");
+        }
+    }
+
+    //Default Color of buttons
+    private void defaultButtonColors() {
+        binding.buttonCelsius.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.blue));
+        binding.buttonFahrenheit.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.blue));
+    }
+    //Update button colors on selecting
+    private void updateButtonColors() {
+        if (isCelsiusSelected) {
+            //Set celsius button to dark blue and fahrenheit button blue
+            binding.buttonCelsius.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.dark_blue));
+            binding.buttonFahrenheit.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.blue));
+        } else {
+            //Set celsius button to  blue and fahrenheit button dark blue
+            binding.buttonFahrenheit.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.dark_blue));
+            binding.buttonCelsius.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.blue));
+        }
+    }
 }
+
